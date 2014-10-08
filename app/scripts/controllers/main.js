32ad1d55ad2ca26d82eb9985b9ba46c167c78f6e
Simon Dufour <sim.dufour@gmail.com>
2014-10-08 08:23:07 -0400

    Initial project version - I just generated with Yeoman

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name cowMilkingClickerApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the cowMilkingClickerApp
+ */
+angular.module('cowMilkingClickerApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
